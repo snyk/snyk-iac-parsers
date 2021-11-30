ff4c40675b5ec7c4c7903caa7917ae86706bcd8e
Murat Celep <murat.celep@snyk.io>
2021-11-30 13:59:07 +0100

    Remove not used context parameter

--- pkg/hcl2.go
+++ pkg/hcl2.go
@@ -1,7 +1,6 @@
 package parsers
 
 import (
-	"context"
 	"encoding/json"
 	"github.com/pkg/errors"
 	hcl2jsonConverter "github.com/tmccombs/hcl2json/convert"
@@ -9,7 +8,7 @@ import (
 
 // ParseHCL2 unmarshals HCL files that are written using
 // version 2 of the HCL language and return parsed file content.
-func ParseHCL2(ctx context.Context, p []byte, v interface{}) (err error) {
+func ParseHCL2(p []byte, v interface{}) (err error) {
 	// TODO: Look into using plain github.com/hashicorp/hcl/v2
 	// instead to avoid the JSON intermediary format.
 
